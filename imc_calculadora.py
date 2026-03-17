835176e1b3dc7e337ddae68c265a9845b1ec871a
Eduardo Oliveira <eduventruee@gmail.com>
2026-03-17 15:23:56 -0300

    Aula 2 - Estruturas de Controle
    Projeto: Sistema de Classificação de IMC

---
+++ imc_calculadora.py
@@ -0,0 +1,18 @@
+#Projeto Sistema de Classificação de IMC
+
+print("Olá, vamos calcular seu IMC?")
+nome = input("Digite seu nome: ")
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+
+imc = peso / (altura**2)
+
+if imc < 18.5:
+    print(f"{nome}, seu IMC é {imc:.2f} e você está abaixo do peso normal. Procure um profissional da saúde para uma avaliação.")
+elif imc >= 18.5 and imc <= 24.9:
+    print(f"{nome}, seu IMC é {imc} e você está com o peso normal.")
+elif imc >= 25 and imc <= 29.9:
+    print(f"{nome}, seu IMC é {imc} e você está com sobrepeso.")
+else:
+    print(f"{nome}, seu IMC é {imc} e você está com obesidade.")
+    print("Você precisa procurar um profissional da saúde para uma consulta.")
